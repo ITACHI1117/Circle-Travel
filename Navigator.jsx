5ebb826af67f74897a2b86ccd3bdecc873be4bef
ITACHI1117 <ajogujoseph0317@gmail.com>
2023-08-21 22:07:18 +0100

    added scrolling and theme

--- Navigator.jsx
+++ Navigator.jsx
@@ -1,17 +1,13 @@
 import * as React from "react";
-import {
-  NavigationContainer,
-  DefaultTheme,
-  DarkTheme,
-} from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Plan from "./screens/Onboarding_Screens/Plan";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { DynamicColorIOS } from "react-native";
-import { Appearance } from "react-native";
+import { Appearance, StatusBar } from "react-native";
+import { useTheme } from "@react-navigation/native";
 
 const Navigator = () => {
   const Stack = createNativeStackNavigator();
+
   // Getting the device default color mode
   const [colorScheme, setColorScheme] = React.useState(
     Appearance.getColorScheme()
@@ -22,17 +18,40 @@ const Navigator = () => {
       setColorScheme(colorScheme)
     );
   }, []);
-  console.log(colorScheme);
+
+  const dark = {
+    dark: true,
+    colors: {
+      primary: "green",
+      background: "black",
+      card: "black",
+      text: "white",
+      border: "blue",
+      notification: "green",
+    },
+  };
+  const light = {
+    dark: false,
+    colors: {
+      primary: "blue",
+      background: "white",
+      card: "green",
+      text: "black",
+      border: "blue",
+      notification: "green",
+    },
+  };
+  //  Getting themes
+  const { colors } = useTheme();
+
   return (
-    <NavigationContainer
-      theme={colorScheme === "dark" ? DarkTheme : DefaultTheme}
-    >
+    <NavigationContainer theme={colorScheme === "dark" ? dark : light}>
+      <StatusBar animated={true} barStyle={colors.text} />
       <Stack.Navigator>
         <Stack.Screen
           name="Plan"
           component={Plan}
           options={{ title: null, headerShown: false }}
-          initialParams={{ color: colorScheme }}
         />
         {/* <Stack.Screen
           name="Home"
--- screens/Onboarding_Screens/Plan.jsx
+++ screens/Onboarding_Screens/Plan.jsx
@@ -1,50 +1,119 @@
-import React, { useEffect } from "react";
-import { TouchableOpacity } from "react-native";
-import { View, Text, StyleSheet, Image } from "react-native";
+import React, { useEffect, useRef } from "react";
+import {
+  Animated,
+  ScrollView,
+  TouchableOpacity,
+  useWindowDimensions,
+} from "react-native";
+import { View, Text, StyleSheet, Image, StatusBar } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useTheme } from "@react-navigation/native";
 
-const Plan = ({ route }) => {
-  const { color } = route.params;
-  console.log(color);
-  // if (colorscheme === "dark") {
-  //   console.log("hi");
-  // }
+const Plan = () => {
+  const { colors } = useTheme();
+  const images = [
+    "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=465&q=80",
+    "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDMHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=465&q=80",
+    "../../assets/undraw_Traveling_yhxq.png",
+  ];
+
+  const data = [
+    {
+      id: 1,
+      img: "../../assets/undraw_adventure_map_hnin.png",
+    },
+    {
+      id: 2,
+      img: "../../assets/undraw_Relaxation_re_ohkx.png",
+    },
+    {
+      id: 3,
+      img: "../../assets/undraw_Traveling_yhxq.png",
+    },
+  ];
+
+  const scrollX = useRef(new Animated.Value(0)).current;
+
+  const { width: windowWidth } = useWindowDimensions();
+
   return (
-    <SafeAreaView>
+    <SafeAreaView
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
       <View style={styles.backView}>
         <View style={styles.header}>
           <TouchableOpacity>
-            <Text style={{ fontSize: 20 }}>Skip</Text>
+            <Text style={{ fontSize: 20, color: colors.text }}>Skip</Text>
           </TouchableOpacity>
         </View>
-        <View
+        <ScrollView
+          horizontal={true}
+          pagingEnabled
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-around",
+            width: windowWidth * 3,
+            height: 300,
+            backgroundColor: "green",
+            marginTop: 30,
+          }}
           style={{
             display: "flex",
-            width: "100%",
-            alignItems: "center",
-            justifyContent: "center",
-            backgroundColor: "white",
+            width: windowWidth,
           }}
         >
-          <Image
-            style={{ width: 250, height: 250 }}
-            source={require("../../assets/undraw_Traveling_yhxq.png")}
-          />
-          <Text style={{ paddingTop: 25, fontSize: 30, fontWeight: 500 }}>
-            Plan Your Trip
-          </Text>
-          <Text
-            style={{
-              width: "80%",
-              paddingTop: 20,
-              fontSize: 25,
-              fontWeight: 200,
-              textAlign: "center",
-            }}
-          >
-            Save places and book your perfect trip with Circle App
-          </Text>
-        </View>
+          {images.map((image) => {
+            return (
+              <View
+                key={image}
+                style={{
+                  alignItems: "center",
+                  justifyContent: "center",
+                  display: "flex",
+                  width: "40%",
+                  height: "100%",
+                  margin: 0,
+                  backgroundColor: "red",
+                }}
+              >
+                <Image
+                  style={{ width: 250, height: 150 }}
+                  source={{
+                    uri: "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=465&q=80",
+                  }}
+                />
+                <Text
+                  style={{
+                    paddingTop: 25,
+                    fontSize: 30,
+                    fontWeight: 500,
+                    color: colors.text,
+                  }}
+                >
+                  Plan your trip
+                </Text>
+                <Text
+                  style={{
+                    width: "80%",
+                    paddingTop: 20,
+                    fontSize: 25,
+                    fontWeight: 200,
+                    textAlign: "center",
+                    color: colors.text,
+                  }}
+                >
+                  Save places and book your perfect trip with Circle App
+                </Text>
+              </View>
+            );
+          })}
+        </ScrollView>
         <View
           style={{
             display: "flex",
@@ -77,8 +146,9 @@ export default Plan;
 const styles = StyleSheet.create({
   backView: {
     display: "flex",
-    justifyContent: "space-around",
-    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "transparent",
     width: "100%",
     height: "100%",
   },
